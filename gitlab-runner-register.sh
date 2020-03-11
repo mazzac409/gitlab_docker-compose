4104ddcb3073180792eb899bba4d96c50bde386a
C. Mazza <45705401+mazzac409@users.noreply.github.com>
2020-03-10 21:13:45 -0500

    Create gitlab-runner-register.sh

---
+++ gitlab-runner-register.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+registration_token="Hr1PyiyJK53amV5DGdvf"
+
+docker exec -it gitlab-runner2 \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description docker-stable \
+    --url http://gitlab-web/ \
+    --executor docker \
+    --docker-image docker:stable \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-network-mode gitlab-network
